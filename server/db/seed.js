59ac3a5bac19ba972ad7bf8a61bb1d35db5a8d33
Jeff <garber34@gmail.com>
2020-10-19 19:35:41 -0400

    users model defined and single seed

---
+++ server/db/seed.js
@@ -0,0 +1,19 @@
+const Users = require('./models/users')
+const db = require('./database')
+
+const seed = async() =>{
+  try {
+  await db.sync({force:true})
+   await Users.create({
+      firstName:"John",
+      email:"test@test.com",
+      hashedPassword:"1234"
+    })
+    console.log("seeded users!")
+  } catch (error) {
+    console.log("ERROR",error)
+  }
+
+}
+
+seed();
